6be1d002834251539667034743265671fc3fe9a6
Markus John <johm@quyo.de>
2022-07-07 12:26:20 +0200

    Improve cachix.sh, add nixbuild.sh, bugfixing

--- overlay.nix
+++ overlay.nix
@@ -1,19 +1,30 @@
 self: super:
 
 let
-  mkShellscriptDerivation = with super; src:
+  mkShellscriptDerivation = with super; src: extraBuildInputs: patchPhase:
     stdenv.mkDerivation {
       name = baseNameOf src;
       inherit src;
-      buildInputs = [ bash ];
+      buildInputs = [ bash ] ++ extraBuildInputs;
+      inherit patchPhase;
       installPhase = "mkdir -p $out/bin && cp * $out/bin/";
     };
 in
 
 {
   # this key should be the same as the flake name attribute.
-  shellscripts-flake = {
-    nix.sh = mkShellscriptDerivation ./nix.sh;
-    cachix.sh = mkShellscriptDerivation ./cachix.sh;
+  shellscripts-flake = with super; {
+
+    nixsh = mkShellscriptDerivation ./nix.sh [] "";
+
+    cachixsh = mkShellscriptDerivation ./cachix.sh [ cachix jq ] ''
+      sed -i -e "s|cachix |${cachix}/bin/cachix |g" cachix-*
+      sed -i -e "s|jq |${jq}/bin/jq |g"             cachix-*
+    '';
+
+    nixbuildsh = mkShellscriptDerivation ./nixbuild.sh [ rlwrap ] ''
+      sed -i -e "s|rlwrap |${rlwrap}/bin/rlwrap |g" nixbuild-*
+    '';
+
   };
 }

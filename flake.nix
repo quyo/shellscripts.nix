b3f3cf3fed2c67f9b0aca0114aa7f0a383d4a1b3
Markus John <johm@quyo.de>
2022-07-08 11:58:11 +0200

    Add devshell

--- flake.nix
+++ flake.nix
@@ -3,15 +3,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "shellscripts-flake";
-      config = { };
-      overlay = ./overlay.nix;
-      systems = [ "x86_64-linux" ];
-    };
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    {
+      overlay = import ./overlay.nix;
+    }
+    //
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlay
+            devshell.overlay
+          ];
+        };
+
+      in rec {
+
+        packages = with pkgs; {
+          inherit cachixsh nixbuildsh nixsh;
+        };
+
+        devShell = pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+
+      }
+    );
 
 }
--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,7 @@
 self: super:
 
 let
+
   mkShellscriptDerivation = with super; src: extraBuildInputs: patchPhase:
     stdenv.mkDerivation {
       name = baseNameOf src;
@@ -9,22 +10,18 @@ let
       inherit patchPhase;
       installPhase = "mkdir -p $out/bin && cp * $out/bin/";
     };
-in
 
-{
-  # this key should be the same as the flake name attribute.
-  shellscripts-flake = with super; {
+in with super; {
 
-    nixsh = mkShellscriptDerivation ./nix.sh [] "";
+  nixsh = mkShellscriptDerivation ./nix.sh [] "";
 
-    cachixsh = mkShellscriptDerivation ./cachix.sh [ cachix jq ] ''
-      sed -i -e "s|cachix |${cachix}/bin/cachix |g" cachix-*
-      sed -i -e "s|jq |${jq}/bin/jq |g"             cachix-*
-    '';
+  cachixsh = mkShellscriptDerivation ./cachix.sh [ cachix jq ] ''
+    sed -i -e "s|cachix |${cachix}/bin/cachix |g" cachix-*
+    sed -i -e "s|jq |${jq}/bin/jq |g"             cachix-*
+  '';
 
-    nixbuildsh = mkShellscriptDerivation ./nixbuild.sh [ rlwrap ] ''
-      sed -i -e "s|rlwrap |${rlwrap}/bin/rlwrap |g" nixbuild-*
-    '';
+  nixbuildsh = mkShellscriptDerivation ./nixbuild.sh [ rlwrap ] ''
+    sed -i -e "s|rlwrap |${rlwrap}/bin/rlwrap |g" nixbuild-*
+  '';
 
-  };
 }

7dd303ec6410656271b1407624e6a66d3859224a
Markus John <johm@quyo.de>
2022-07-22 22:23:18 +0200

    Use wrapProgram for PATH prefixing

--- overlay.nix
+++ overlay.nix
@@ -2,34 +2,46 @@ version: final: prev:
 
 let
 
-  mkShellscriptDerivation = src: extraBuildInputs:
+  mkShellscriptDerivation = src: buildInputs:
     final.stdenv.mkDerivation ({
       pname = baseNameOf src;
       inherit version;
       inherit src;
-      buildInputs = [ final.bash ] ++ (builtins.attrValues extraBuildInputs);
-      patchPhase = "for file in * ; do substituteAllInPlace \"$file\" ; done";
-      installPhase = "mkdir -p $out/bin && cp * $out/bin/";
+
+      nativeBuildInputs = [ final.makeWrapper ];
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/bin
+        cp * $out/bin/
+
+        for file in $out/bin/* ; do
+          wrapProgram $file \
+            --prefix PATH : ${final.lib.makeBinPath (builtins.attrValues buildInputs)}
+        done
+
+        runHook postInstall
+      '';
 
       doInstallCheck = true;
       installCheckPhase = ''
+        runHook preInstallCheck
+
         for file in $out/bin/* ; do
           ${final.stdenv.shellDryRun} "$file"
           ${final.shellcheck}/bin/shellcheck "$file"
         done
-      '';
-    }
-    //
-    extraBuildInputs);
 
-  # substituteAllInPlace doesn't work on @nix-tree@
-  nixtree = final.nix-tree;
+        runHook postInstallCheck
+      '';
+    });
 
 in with final; {
 
   cachixsh = mkShellscriptDerivation ./cachix.sh { inherit cachix findutils git jq nix openssh; };
   dockersh = mkShellscriptDerivation ./docker.sh { inherit docker git nix openssh; };
-  nixsh = mkShellscriptDerivation ./nix.sh { inherit coreutils findutils git gnugrep nix nixtree openssh; };
+  nixsh = mkShellscriptDerivation ./nix.sh { inherit coreutils findutils git gnugrep nix nix-tree openssh; };
   nixbuildsh = mkShellscriptDerivation ./nixbuild.sh { inherit git nix openssh rlwrap; };
 
 }

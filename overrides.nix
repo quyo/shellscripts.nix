2d977ab253ac63ec17a54b03f70f5a2cd2af43c9
Markus John <johm@quyo.de>
2022-09-05 07:34:33 +0000

    Add more armv7l-linux fixes

--- overrides.nix
+++ overrides.nix
@@ -18,8 +18,18 @@ in
 
   haskellPackages = prev.haskellPackages.extend (hfinal: hprev: {
     bsb-http-chunked = dontCheckHaskell hprev.bsb-http-chunked;
+    cborg = dontCheckHaskell (hprev.cborg.overrideAttrs (oldAttrs: {
+      p296 = ./overrides-cborg-p296.patch;
+      postPatch = oldAttrs.postPatch or "" + ''
+        patch -p2 <$p296
+      '';
+    }));
     half = dontCheckHaskell hprev.half;
+    inline-c = dontCheckHaskell hprev.inline-c;
+    inline-c-cpp = dontCheckHaskell hprev.inline-c-cpp;
+    insert-ordered-containers = dontCheckHaskell hprev.insert-ordered-containers;
     relude = dontCheckHaskell hprev.relude;
+    serialise = dontCheckHaskell hprev.serialise;
     th-orphans = dontCheckHaskell hprev.th-orphans;
     time-compat = dontCheckHaskell hprev.time-compat;
   });
